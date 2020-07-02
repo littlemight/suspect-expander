9bffb1a4a57e3d5685bb49d991d0334ad4c27a24
Michel Fang <mfang555@gmail.com>
2020-07-02 20:43:00 +0700

    Updates

---
+++ precache-manifest.c6b6a00b5402739374d18150c68a64d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce39d915994d0d33fff2acec9b7e12f",
+    "url": "/suspect-expander/index.html"
+  },
+  {
+    "revision": "c45aef466d6d8e1bcd33",
+    "url": "/suspect-expander/static/css/main.8236b94f.chunk.css"
+  },
+  {
+    "revision": "820aa5be14af63303206",
+    "url": "/suspect-expander/static/js/2.cc5bcf07.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/suspect-expander/static/js/2.cc5bcf07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c45aef466d6d8e1bcd33",
+    "url": "/suspect-expander/static/js/main.d2a27442.chunk.js"
+  },
+  {
+    "revision": "792fa51cd76d501a7984",
+    "url": "/suspect-expander/static/js/runtime-main.509d4b03.js"
+  }
+]);
